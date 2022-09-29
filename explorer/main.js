f722165b3b4be9c2c1d32b15b533e32fb863998f
TemidayoOluwa <106041786+TemidayoOluwa@users.noreply.github.com>
2022-09-29 19:55:09 +0100

    xeraxlab internship assignment 1

---
+++ explorer/main.js
@@ -0,0 +1,7 @@
+var hamburger = document.querySelector(".hamburger-container")
+var mobilelinkscontainer = document.querySelector(".mobile-links-container")
+
+function openmenu(){
+    hamburger.classList.toggle("showburger")
+    mobilelinkscontainer.classList.toggle("showmobilelinks")
+}
